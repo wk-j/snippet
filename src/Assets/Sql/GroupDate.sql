7a99efdb207c3ec7cfd92f7099437bca3772d566
wk-j <somnuk.wk@gmail.com>
2019-02-27 16:35:39 +0700

    Validated

--- src/Assets/Sql/GroupDate.sql
+++ src/Assets/Sql/GroupDate.sql
@@ -6,11 +6,18 @@ WHERE "AlfrescoUuid" <> '' AND "UploadDate"  BETWEEN '2018-01-01' AND  '2019-12-
 GROUP BY Date
 ORDER BY Date
 
-
 -- Group by Create Date
 
 SELECT count(*), to_char("CreateDate", 'YYYY/MM/dd') AS Date
 FROM "QFiles"
 WHERE "CreateDate"  BETWEEN '2018-01-01' AND  '2019-12-31'
+GROUP BY Date
+ORDER BY Date
+
+-- Validated
+
+SELECT count(*), to_char("CreateDate", 'YYYY/MM/dd') AS Date
+FROM "QFiles"
+WHERE "Status" = 2
 GROUP BY Date
 ORDER BY Date
